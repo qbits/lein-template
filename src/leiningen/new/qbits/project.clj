fe09dbe84ede7afd78c06c5a949bb3c6e3f18496
Max Penet <m@qbits.cc>
2013-04-04 12:39:08 +0200

    update dependencies

--- src/leiningen/new/qbits/project.clj
+++ src/leiningen/new/qbits/project.clj
@@ -3,7 +3,9 @@
   :url "https://github.com/mpenet/{{name}}"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.4.0"]]
+  :dependencies [[org.clojure/clojure "1.5.1"]
+                 [org.clojure/tools.logging "0.2.6"]
+                 [slingshot "0.10.3"]]
   :profiles {:1.4  {:dependencies [[org.clojure/clojure "1.4.0"]]}
              :1.5  {:dependencies [[org.clojure/clojure "1.5.0"]]}
              :1.6  {:dependencies [[org.clojure/clojure "1.6.0-master-SNAPSHOT"]]}
