6e82f971adbe6d42b2175cbb8c120b8754321ac2
Max Penet <m@qbits.cc>
2014-05-19 13:25:44 +0200

    bump

--- src/leiningen/new/qbits/project.clj
+++ src/leiningen/new/qbits/project.clj
@@ -4,7 +4,7 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [com.gfredericks/catch-data "0.1.0"]]
+                 [com.gfredericks/catch-data "0.1.1"]]
   :profiles {:1.4  {:dependencies [[org.clojure/clojure "1.4.0"]]}
              :1.5  {:dependencies [[org.clojure/clojure "1.5.0"]]}
              :1.6  {:dependencies [[org.clojure/clojure "1.6.0-master-SNAPSHOT"]]}
