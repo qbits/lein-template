65bea4c79235072dbcf694f1d8caa38b9f7bc054
Max Penet <m@qbits.cc>
2014-12-03 18:05:29 +0100

    update project template with latest dependencies

--- src/leiningen/new/qbits/project.clj
+++ src/leiningen/new/qbits/project.clj
@@ -3,11 +3,12 @@
   :url "https://github.com/mpenet/{{name}}"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.1"]
-                 [com.gfredericks/catch-data "0.1.1"]]
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 [com.gfredericks/catch-data "0.1.3"]]
   :profiles {:1.4  {:dependencies [[org.clojure/clojure "1.4.0"]]}
              :1.5  {:dependencies [[org.clojure/clojure "1.5.0"]]}
-             :1.6  {:dependencies [[org.clojure/clojure "1.6.0-master-SNAPSHOT"]]}
+             :1.6  {:dependencies [[org.clojure/clojure "1.6.0"]]}
+             :1.7  {:dependencies [[org.clojure/clojure "1.7.0-master-SNAPSHOT"]]}
              :dev  {:dependencies [[codox "0.8.10"]]}
              :test  {:dependencies []}}
 
