1ae09acce9f49cfcb758bb1de8dd3d9dff7d0b77
Max Penet <m@qbits.cc>
2016-01-15 15:53:51 +0100

    update

--- src/leiningen/new/component/project.clj
+++ src/leiningen/new/component/project.clj
@@ -1,7 +1,7 @@
 (defproject cc.qbits.component/{{name}} "0.1.0-SNAPSHOT"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[com.stuartsierra/component "0.2.3"]
-                 [prismatic/schema "0.4.1"]]
+  :dependencies [[com.stuartsierra/component "0.3.1"]
+                 [prismatic/schema "1.0.4"]]
   :source-paths ["src/clj"]
   :global-vars {*warn-on-reflection* true})
--- src/leiningen/new/qbits/project.clj
+++ src/leiningen/new/qbits/project.clj
@@ -3,24 +3,19 @@
   :url "https://github.com/mpenet/{{name}}"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.7.0"]
+  :dependencies [[org.clojure/clojure "1.8.0-RC5"]
                  ;; [com.gfredericks/catch-data "0.2.0"]
-                 ;; [com.stuartsierra/component "0.3.0"]
-                 ;; [prismatic/schema "0.4.0"]
+                 ;; [com.stuartsierra/component "0.3.1"]
+                 ;; [prismatic/schema "1.0.4"]
                  ]
-  :profiles {:1.4  {:dependencies [[org.clojure/clojure "1.4.0"]]}
-             :1.5  {:dependencies [[org.clojure/clojure "1.5.0"]]}
-             :1.6  {:dependencies [[org.clojure/clojure "1.6.0"]]}
-             :1.7  {:dependencies [[org.clojure/clojure "1.7.0-master-SNAPSHOT"]]}
-             :dev  {:dependencies [[codox "0.8.10"]]}
+  :profiles {:dev  {:dependencies []}
              :test  {:dependencies []}}
 
-  :codox {:src-dir-uri "https://github.com/mpenet/{{name}}/blob/master/"
-          :src-linenum-anchor-prefix "L"
-          :defaults {:doc/format :markdown}
-          :output-dir "doc/codox"}
+  :codox {:source-uri "https://github.com/mpenet/{{name}}/blob/master/{filepath}#L{line}"
+          :metadata {:doc/format :markdown}
+          :namespaces :all}
 
-    :source-paths ["src/clj"]
-    :java-source-paths ["src/java"]
-    ;; :javac-options ["-source" "1.6" "-target" "1.6" "-g"]
-    :global-vars {*warn-on-reflection* true})
+  :source-paths ["src/clj"]
+  :java-source-paths ["src/java"]
+  ;; :javac-options ["-source" "1.6" "-target" "1.6" "-g"]
+  :global-vars {*warn-on-reflection* true})
