d607011d040647415cd9baae19438de8a22ebae1
Max Penet <m@qbits.cc>
2017-03-31 13:10:36 +0200

    bump

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject qbits/lein-template "1.2.0"
+(defproject qbits/lein-template "1.3.0"
   :description "Tempate for qbits projects"
   :url "http://qbits.cc"
   :license {:name "Eclipse Public License"
